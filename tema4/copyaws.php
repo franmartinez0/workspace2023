835aa708dbc2b5bf8ae112dfffd2b04e5610d3be
EC2 Default User <ec2-user@ip-172-31-36-41.ec2.internal>
2023-10-18 18:34:30 +0000

    awsrds

---
+++ tema4/copyaws.php
@@ -0,0 +1,38 @@
+
+<?php
+ $dbh = null;
+
+ define('DB_NAME', 'php');
+ define('DB_USER', 'admin');
+ define('DB_PASSWORD', 'servidor2024');
+ define('DB_HOST', 'database-php.cj1wbbjnneg8.us-east-1.rds.amazonaws.com'); //La IP del contenedor Mysql, y el puerto interno del contenedor , no tiene nada que ver con el puerto que toma el contenedor para conectarse al exterior
+
+ try {
+     $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
+     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+ } catch (PDOException $e){
+     echo $e->getMessage();
+ }
+
+//preparamos la consulta -> llamada a metodos
+ $stmt = $dbh->prepare("SELECT * FROM usuarios ");
+ // Especificamos el fetch mode antes de llamar a fetch()
+ //con fetch aasoc devuelve los datos en un array asociativo
+ $stmt->setFetchMode(PDO::FETCH_ASSOC);
+ // Ejecutamos
+ $stmt->execute();
+
+
+ //cada vez que llamamos a fetch nos devuelve una fila
+
+ //las claves del array asociativo son las columnas de la tabla
+ 
+ while($row = $stmt->fetch()){
+     echo "Nombre: " . $row['nombre'] . "<br>";
+     echo "Email: " . $row['email'] . "<br>";
+
+ }
+
+
+?>
