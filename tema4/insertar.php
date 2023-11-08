c0a33aeb884efb18d5169193b0668efd33598287
franmartinez0 <fran.martinezlopez29398@gmail.com>
2023-11-08 20:10:32 +0100

    host mariadb

--- tema4/insertar.php
+++ tema4/insertar.php
@@ -4,7 +4,8 @@
  define('DB_NAME', 'php');
  define('DB_USER', 'root');
  define('DB_PASSWORD', 'toor');
- define('DB_HOST', '172.17.0.2:3306'); //La IP del contenedor Mysql, y el puerto interno del contenedor , no tiene nada que ver con el puerto que toma el contenedor para conectarse al exterior
+ define('DB_HOST', 'mariadb'); //La IP del contenedor Mysql, y el puerto interno del contenedor , no tiene nada que ver con el puerto que toma el contenedor para conectarse al exterior
+ //al tener el contenedor con poner el nombre del contenedor de base de datos en el host es suficiente
 
  try {
      $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
