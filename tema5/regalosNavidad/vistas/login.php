f16219472b70ea4f696d2bce3d4f4cd1ba96e40a
franmartinez0 <fran.martinezlopez29398@gmail.com>
2023-11-10 15:11:22 +0100

    ....

--- tema5/regalosNavidad/vistas/login.php
+++ tema5/regalosNavidad/vistas/login.php
@@ -3,7 +3,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <div class='container'>
 
-    <form class="form-signin  mt-2 text-center py-3 " action="controladorLogin.php" method="POST">
+    <form class="form-signin  mt-2 text-center py-3 " action="controladorUsuario.php" method="POST">
       <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
       <h1 class="h3 mb-3 font-weight-normal">Log in </h1>
       <label for="inputEmail" class="sr-only">Email:</label>
