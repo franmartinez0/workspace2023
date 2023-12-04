d2ff52a4710c948bd2aad23d006b2b29d26f79fb
franmartinez0 <fran.martinezlopez29398@gmail.com>
2023-12-04 16:38:33 +0100

    apinodejs

--- tema6/api/api.php
+++ tema6/api/api.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,31 +9,42 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
 </head>
+
 <body>
     <div class='container'>
 
         <h3 class='mb-3'>Valorant</h3>
 
         <div class='row'>
-    
-<?php
 
-//echo $resultado;
-$uri = "https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos?sol=1000&api_key=";       
-$reqPrefs['http']['method'] = 'GET';
-$reqPrefs['http']['header'] = 'nXutjGN05a7Q9V49spoDZVjf2RfTdAHGFmgTLZQP';
-$stream_context = stream_context_create($reqPrefs);
-$resultado = file_get_contents($uri, false, $stream_context);
+            <?php
+
+            //echo $resultado;
+            $uri = "https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos?sol=1000&api_key=";
+            $reqPrefs['http']['method'] = 'GET';
 
 
 
 
-//Pasar de json a objeto php y recorrer los resultados
-if ($resultado != false) {
-    $respPHP = json_decode($resultado);
+            $reqPrefs['http']['header'] = 'nXutjGN05a7Q9V49spoDZVjf2RfTdAHGFmgTLZQP';
+            $stream_context = stream_context_create($reqPrefs);
+            $resultado = file_get_contents($uri, false, $stream_context);
 
-    foreach($respPHP->data as $agente) {
-        echo "
+
+
+
+            //Pasar de json a objeto php y recorrer los resultados
+            if ($resultado != false) {
+                $respPHP = json_decode($resultado);
+
+                foreach ($respPHP->data as $agente) {
+
+
+
+
+
+
+                    echo "
         <div class='card' style='width: 18rem ;'>
             <img src='{$agente->fullPortrait}' class='card-img-top' alt='...'>
             <div class='card-body'>
@@ -45,15 +57,14 @@ if ($resultado != false) {
             </div>
         </div>
         ";
+                }
+            }
+            ?>
 
 
-    }
-}
-?>
-        
-
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
 
 </body>
+
 </html>
